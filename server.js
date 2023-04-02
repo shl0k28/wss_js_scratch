4e0124045ecd401bc3dd69d1e47d521ff0903a41
Shlok D. Mange <shlok_mange@protonmail.com>
2023-04-02 12:00:06 +0530

    exception error handler

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const { createServer } = require('http')
 const PORT = process.env.PORT || 8080 
 
+const errors = ["uncaughtException","unhandledRejection"]
+errors.forEach(event => {
+    process.on(event, (err) => {
+        console.error(`Event: ${event}, msg: ${err.stack || err}`)
+    })
+})
 createServer((req, res) => {
     res.writeHead(200)
     res.end()
