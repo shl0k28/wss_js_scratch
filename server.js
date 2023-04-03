440977772f9b9e398dd43f7954f29328de84b0f2
Shlok D. Mange <shlok_mange@protonmail.com>
2023-04-03 20:57:12 +0530

    get client socket header

--- server.js
+++ server.js
@@ -1,13 +1,32 @@
-const { createServer } = require('http')
-const PORT = process.env.PORT || 8080 
+const { createServer, request } = require('http')
+const { createHash } = require('crypto')
 
-const errors = ["uncaughtException","unhandledRejection"]
+const PORT = process.env.PORT || 8080 
+const WEBSOCKET_MAGIC_STRING = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11'
+
+const errors = [
+    "uncaughtException",
+    "unhandledRejection"
+]
 errors.forEach(event => {
     process.on(event, (err) => {
         console.error(`Event: ${event}, msg: ${err.stack || err}`)
     })
 })
-createServer((req, res) => {
+
+const server = createServer((req, res) => {
     res.writeHead(200)
     res.end()
-}).listen(PORT, () => console.log(`Server listening on port: ${PORT}`))
+})
+
+server.listen(PORT, () => console.log(`Server listening on port: ${PORT}`))
+
+const getClientWebsocketKey = async () => {
+
+}
+
+const getClientSocketHeader = async (key) => {
+    const sha1 = createHash()
+    sha1.update(key + WEBSOCKET_MAGIC_STRING)
+    return sha1.digest('base64')
+}
